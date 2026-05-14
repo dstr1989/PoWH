a8d0d888fbb25142cfae615135a4ec757074a764
dstr1989 <99421580+dstr1989@users.noreply.github.com>
2026-05-14 15:35:21 +0200

    Zmiana clientversion.h na PoWH: wersja 1.0.0, zmiana nagłówków i copyright

--- src/clientversion.h
+++ src/clientversion.h
@@ -1,54 +1,27 @@
-// Copyright (c) 2009-2016 The Bitcoin Core developers
-// Copyright (c) 2017-2019 The Raven Core developers
+// Copyright (c) 2009-2026 The PoWH Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef RAVEN_CLIENTVERSION_H
-#define RAVEN_CLIENTVERSION_H
+#ifndef POWH_CLIENTVERSION_H
+#define POWH_CLIENTVERSION_H
 
 #if defined(HAVE_CONFIG_H)
-#include "config/raven-config.h"
-#endif //HAVE_CONFIG_H
-
-// Check that required client information is defined
-#if !defined(CLIENT_VERSION_MAJOR) || !defined(CLIENT_VERSION_MINOR) || !defined(CLIENT_VERSION_REVISION) || !defined(CLIENT_VERSION_BUILD) || !defined(CLIENT_VERSION_IS_RELEASE) || !defined(COPYRIGHT_YEAR)
-#error Client version information missing: version is not defined by raven-config.h or in any other way
+#include "config/powh-config.h"
 #endif
 
+// Client version number
+#define CLIENT_VERSION_MAJOR 1
+#define CLIENT_VERSION_MINOR 0
+#define CLIENT_VERSION_REVISION 0
+#define CLIENT_VERSION_BUILD 0
+
+//! Set to true for release, false for prerelease or test build
+#define CLIENT_VERSION_IS_RELEASE true
+
 /**
- * Converts the parameter X to a string after macro replacement on X has been performed.
- * Don't merge these into one macro!
+ * Copyright year (2009-this)
+ * Todo: update this when changing our copyright comments in the source
  */
-#define STRINGIZE(X) DO_STRINGIZE(X)
-#define DO_STRINGIZE(X) #X
+#define COPYRIGHT_YEAR 2026
 
-//! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " " COPYRIGHT_HOLDERS_FINAL
-
-/**
- * ravend-res.rc includes this file, but it cannot cope with real c++ code.
- * WINDRES_PREPROC is defined to indicate that its pre-processor is running.
- * Anything other than a define should be guarded below.
- */
-
-#if !defined(WINDRES_PREPROC)
-
-#include <string>
-#include <vector>
-
-static const int CLIENT_VERSION =
-                           1000000 * CLIENT_VERSION_MAJOR
-                         +   10000 * CLIENT_VERSION_MINOR
-                         +     100 * CLIENT_VERSION_REVISION
-                         +       1 * CLIENT_VERSION_BUILD;
-
-extern const std::string CLIENT_NAME;
-extern const std::string CLIENT_BUILD;
-
-
-std::string FormatFullVersion();
-std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments);
-
-#endif // WINDRES_PREPROC
-
-#endif // RAVEN_CLIENTVERSION_H
+#endif // POWH_CLIENTVERSION_H
